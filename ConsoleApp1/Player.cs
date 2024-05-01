41e812916837fcea44e2fc6a5e0e46f5272136fb
hongzina <ghdjindk1350@gmail.com>
2024-05-01 15:43:40 +0900

    캐릭터 생성 수정
    .

--- ConsoleApp1/Player.cs
+++ ConsoleApp1/Player.cs
@@ -63,4 +63,43 @@ internal class Player
             Def++;
         }
     }
+    public Player PlayerCreate(Player player)
+    {
+        
+        Console.Clear();
+        ConsoleUtility.MakeSentence("\n당신을 환영합니다!");
+        ConsoleUtility.MakeSentence("\n");
+        ConsoleUtility.MakeSentence("\n당신의 이름은 무엇인가요?");
+        ConsoleUtility.MakeSentence("\n");
+        player.Name = Console.ReadLine();
+        ConsoleUtility.MakeSentence($"\n그렇군요.{player.Name}, 만나서 반갑습니다.");
+        ConsoleUtility.MakeSentence("\n당신의 직업은 무엇인가요?");
+        ConsoleUtility.MakeSentence("\n");
+        ConsoleUtility.MakeSentence("\n1. 전사");
+        ConsoleUtility.MakeSentence("\n2. 기사");
+        ConsoleUtility.MakeSentence("\n3. 궁수");
+        ConsoleUtility.MakeSentence("\n");
+        ConsoleUtility.MakeSentence("\n");
+        int choice = ConsoleUtility.PromotMenuChoice(1, 3);
+
+        switch (choice)
+        {
+            case 1:
+                player.Job = "전사";
+                break;
+            case 2:
+                player.Job = "기사";
+                break;
+            case 3:
+                player.Job = "궁수";
+                break;
+        }
+
+        ConsoleUtility.MakeSentence("\n");
+        ConsoleUtility.MakeSentence("\n그렇군요. 감사합니다.");
+        ConsoleUtility.MakeSentence("\n이제 당신이 누구인지 알겠습니다.");
+        ConsoleUtility.MakeSentence("\n그러면 모험을 떠나볼까요?");
+        Console.ReadKey();
+        return player;
+    }
 }
